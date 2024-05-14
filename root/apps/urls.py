732891ce688b54086f9be6b949de4be53c62f2dc
msi <meibrohiomv@gmial.com>
2024-05-15 04:23:49 +0500

    new delete html

--- root/apps/urls.py
+++ root/apps/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, UserDeleteView
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    # path('delete/<int:pk>', DeletePeople.as_view(), name='delete'),
+    path('delete/<int:pk>', UserDeleteView.as_view(), name='delete'),
 ]
